463979aeba611073c9ba1fbd447f73ea7b406fc0
Leandro <leandro.fuenzalida@gmail.com>
2026-08-07 21:54:35 -0400

    Agrega jQuery: animacion en menu, mensaje dinamico en deposito, filtro de contactos

--- js/menu.js
+++ js/menu.js
@@ -2,3 +2,8 @@
 
 const saldoTexto = document.getElementById("saldo");
 saldoTexto.textContent = formatearSaldo(obtenerSaldo());
+
+// Animación simple con jQuery al cargar el menú
+$(function () {
+  $(".bg-primary").hide().fadeIn(600);
+});
